29eb1f74eb5d4fcb75dd91f138922abc76c3ebf8
daniel glenn <linuxbender@gmail.com>
2017-04-19 22:47:15 +0200

    list view with for-loop example

--- app/src/main/java/ch/theforce/miwok/NumbersActivity.java
+++ app/src/main/java/ch/theforce/miwok/NumbersActivity.java
@@ -1,7 +1,12 @@
 package ch.theforce.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        List<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for (int index = 0; index < words.size(); index++) {
+            TextView textView = new TextView(this);
+            textView.setText(words.get(index));
+            rootView.addView(textView);
+            index++;
+        }
     }
 }
