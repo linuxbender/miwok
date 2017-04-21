880633ecc997623b38eec34c9216baca446774c1
daniel glenn <linuxbender@gmail.com>
2017-04-21 16:15:26 +0200

    Insert translation and replace activities with new word_list

--- app/src/main/java/ch/theforce/miwok/WordAdapter.java
+++ app/src/main/java/ch/theforce/miwok/WordAdapter.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class WordAdapter extends ArrayAdapter<Word> {
 
     public WordAdapter(@NonNull Context context, @NonNull List<Word> objects) {
-        super(context, 0, objects);
+        super(context, R.layout.list_item, objects);
     }
 
     @NonNull
