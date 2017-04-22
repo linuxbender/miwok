6a31f26b4c304d005186b9f87fe9e24b60ab2e34
daniel glenn <linuxbender@gmail.com>
2017-04-22 03:14:46 +0200

    Extends word object and register a sound resources

--- app/src/main/java/ch/theforce/miwok/Word.java
+++ app/src/main/java/ch/theforce/miwok/Word.java
@@ -3,25 +3,30 @@ package ch.theforce.miwok;
 public class Word {
 
     private static final int NO_IMAGE_PROVIDED = -1;
+    private static final int NO_SOUND_PROVIDED = -1;
     private final String miwokTranslation;
     private final String defaultTranslation;
     private final int imageResourceId;
+    private final int soundResourceId;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int soundResourceId) {
         this.miwokTranslation = miwokTranslation;
         this.defaultTranslation = defaultTranslation;
         this.imageResourceId = NO_IMAGE_PROVIDED;
+        this.soundResourceId = soundResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int soundResourceId) {
         this.miwokTranslation = miwokTranslation;
         this.defaultTranslation = defaultTranslation;
         this.imageResourceId = imageResourceId;
+        this.soundResourceId = soundResourceId;
     }
 
     public String getMiwokTranslation() {
         return miwokTranslation;
     }
+
     public String getDefaultTranslation() {
         return defaultTranslation;
     }
@@ -30,7 +35,15 @@ public class Word {
         return imageResourceId;
     }
 
+    public int getSoundResourceId() {
+        return soundResourceId;
+    }
+
     public boolean hasImage() {
         return this.getImageResourceId() != NO_IMAGE_PROVIDED;
+    }
+
+    public boolean hasSound() {
+        return this.getSoundResourceId() != NO_SOUND_PROVIDED;
     }
 }
