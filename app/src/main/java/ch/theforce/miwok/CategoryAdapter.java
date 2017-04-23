aef11edf5be663afd85893ab301c9cda31659e1c
daniel glenn <linuxbender@gmail.com>
2017-04-24 00:49:12 +0200

    Impl. ViewPager

---
+++ app/src/main/java/ch/theforce/miwok/CategoryAdapter.java
@@ -0,0 +1,42 @@
+package ch.theforce.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = new NumbersFragment();
+                break;
+            case 1:
+                fragment = new FamilyFragment();
+                break;
+            case 2:
+                fragment = new ColorsFragment();
+                break;
+            case 3:
+                fragment = new PhrasesFragment();
+                break;
+            default:
+                fragment = null;
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
--- app/src/main/java/ch/theforce/miwok/MainActivity.java
+++ app/src/main/java/ch/theforce/miwok/MainActivity.java
@@ -1,57 +1,20 @@
 package ch.theforce.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        // Find the View that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
+        CategoryAdapter categoryAdapter = new CategoryAdapter(getSupportFragmentManager());
 
-        // Set a click listener on that View
-        numbers.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent numberActivity = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(numberActivity);
-            }
-        });
-
-        TextView familyMembers = (TextView) findViewById(R.id.family);
-        familyMembers.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent familyMembers = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(familyMembers);
-            }
-        });
-
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent phrases = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(phrases);
-            }
-        });
-
-        TextView colors = (TextView) findViewById(R.id.colors);
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent colors = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(colors);
-            }
-        });
+        mViewPager.setAdapter(categoryAdapter);
     }
 }
